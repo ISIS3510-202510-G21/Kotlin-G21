1b78c6333a7ca5a896bc08c312cb474c7eb70a41
luna-as18 <m.lunav@uniandes.edu.co>
2025-03-12 11:16:54 -0500

    Model Auth Objects, plus MVVM esqueleton

---
+++ app/src/main/java/com/isis3510/growhub/model/objects/AppUser.kt
@@ -0,0 +1,8 @@
+package com.isis3510.growhub.model.objects
+
+data class AppUser(
+    val email: String = "",
+    val name: String = "",
+    val userType: String = "host", // Valor por defecto
+    val username: String = ""      // Si en un futuro decides usarlo
+)
