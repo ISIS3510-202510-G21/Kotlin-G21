fce9707a565dc774afc7c402c37aa3fcba3e645d
Nicolás Rincón Sánchez <98567940+nrincon2302@users.noreply.github.com>
2025-03-19 10:39:45 -0500

    6 implementation of home view (#11)
    * Created home cards but still no format
    
    * Top box added with formatting - No MUI yet
    
    * Home almost ready
    
    * Added icons not included in MUI
    
    * First try at the navbar
    
    * Final automatization DONE
    
    * its working
    
    * Almost done with the Home fixes
    
    * Added support for landscape home, not the best but ok
    
    * Home pretty much done

--- app/src/main/java/com/isis3510/growhub/MainActivity.kt
+++ app/src/main/java/com/isis3510/growhub/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.FirebaseApp
 import com.isis3510.growhub.view.navigation.AppNavGraph
 import com.isis3510.growhub.view.navigation.Destinations
 import com.isis3510.growhub.viewmodel.AuthViewModel
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        FirebaseApp.initializeApp(this)
         enableEdgeToEdge()
 
         setContent {
@@ -51,7 +53,7 @@ fun AppScaffold(startDestination: String, authViewModel: AuthViewModel) {
         AppNavGraph(
             navController = navController,
             startDestination = startDestination,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
         )
     }
 }
