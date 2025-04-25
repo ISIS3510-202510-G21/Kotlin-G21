c761a09246fb487f9668acec6514c3e5a332218a
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2025-04-25 10:02:57 -0500

    Fix Location

--- app/src/main/java/com/isis3510/growhub/model/objects/Location.kt
+++ app/src/main/java/com/isis3510/growhub/model/objects/Location.kt
@@ -1,8 +1,22 @@
 package com.isis3510.growhub.model.objects
 
+import com.google.android.gms.maps.model.LatLng
+
 data class Location(
+    val id: String,
     val address : String = "",
-    val city: String = "Bogotá",
+    val city: String = "",
     val details: String = "",
+    val latitude: Double,
+    val longitude: Double,
     val university: Boolean = true,
 )
+{
+    fun getInfo(): String {
+        return "$address, $city"
+    }
+
+    fun getCoordinates(): LatLng {
+        return LatLng(latitude, longitude)
+    }
+}
